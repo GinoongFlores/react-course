5c9d45c091825bea4ff701b1c6e0b305791a3b0e
ginoongflores <florespaul161@gmail.com>
2023-02-01 19:39:53 +0800

    finalize the app
    finalize the app in a single component for now

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,50 @@ function App() {
 	const [newTask, setNewTask] = useState("");
 
 	const handleChange = (event) => {
-		setNewTask(event.target.value); // setNewTask is a function that sets the value of newTask
+		setNewTask(event.target.value); // setNewTask is a function that sets the value of newTask. The event target value gets the user input and we passed into the setNewTask function to set the value of newTask
 	};
 
 	const addTasks = () => {
-		const newTodoList = [...todoList, newTask]; // here we are copying everything from the todoList and adding the newTask to it
-		setTodoList(newTodoList); // here we set the newTodoList function to the newTodoList
-		// console.log(newTodoList);
+		// the task here is an object that sets an id and task name to a task
+		const task = {
+			// here we want to grab the element in the array before the current one and add 1 to that id
+			id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1, // again grabbing the last element of the array and adding one to it to increment the id by one. But, if the array has no id then we want to set it to 1
+			taskName: newTask,
+		};
+
+		setTodoList([...todoList, task]); // here we are copying everything from the todoList and adding the new task to it and then setting it to the todoList
+	};
+
+	const deleteTask = (id) => {
+		// better and shorter way of doing it
+		//if the task.id is not equal to the id that we just passed then we want to delete it
+		setTodoList(todoList.filter((task) => task.id !== id));
 	};
 
 	return (
-		<div className="App">
+		<div className="todoList">
 			<div className="addTask">
 				<input type="text" onChange={handleChange} />
-				<button onClick={addTasks}>Add Task</button>
+				<button onClick={addTasks}>Add a Task</button>
 			</div>
 
 			{/* Lists of added tasks */}
-			<div className="list">
-				{todoList.map((task) => {
-					return <h1>{task}</h1>;
-				})}
+
+			<div className="todoList--tasks">
+				<h2>Added Tasks</h2>
+				<div className="list">
+					{todoList.map((task) => {
+						// here the map loops through the todoList and returns the task
+						return (
+							<div className="taskLists">
+								<h3>{task.taskName}</h3>
+
+								{/* here we are calling a function that the deletesTask function. And has the parameter of the task.id from the task object because we want to delete the task now by its id. */}
+								<button onClick={() => deleteTask(task.id)}>X</button>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
