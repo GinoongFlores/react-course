3ad852509c1e1e793524c69893c443bfd20bfa2c
ginoongflores <florespaul161@gmail.com>
2023-02-01 11:22:14 +0800

    feat(useState): increase age

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Planets from "./Planets";
+
+import { useState } from "react";
+// A state in react is a variable that stores data that can be changed
 
 function App() {
-	const planets = [
-		{ name: "Mars", isGasPlanet: false },
-		{ name: "Earth", isGasPlanet: false },
-		{ name: "Jupiter", isGasPlanet: true },
-		{ name: "Venus", isGasPlanet: false },
-		{ name: "Neptune", isGasPlanet: true },
-		{ name: "Uranus", isGasPlanet: true },
-	];
+	// let age = 0;
+	let [age, setAge] = useState(0); // the age is a variable that sets value and the setAge is the function that lets the age variable to be changed
+
+	// Here we cannot change the value of age because react only rerender once. And React don't know that the value of age has changed and it will just display the default value of age which is 0. So we must use State to change the value of age.
+	const increaseAge = () => {
+		setAge(age + 1); // the age will change depending on the setAge function
+		// age++;
+		// document.getElementById("age").innerHTML = age; this is an old way of doing it in Javascript
+		console.log(age);
+	};
 
 	return (
 		<div className="App">
-			<>
-				{/* Objective: Display only gas planets */}
-				<h1>Gas Planets - using props</h1>
-				{planets.map((planet, key) => {
-					{
-						/* sample: using if statement */
-						{
-							/* if (planet.isGasPlanet) return <h1>{planet.name}</h1>; */
-						}
-					}
-
-					return (
-						<div>
-							{/* Another example: <h2 key={key}>{planet.isGasPlanet && planet.name}</h2> */}
-							<Planets key={key} planets={planet.isGasPlanet && planet.name} />
-						</div>
-					);
-				})}
-
-				<h1>Gas Planet - using map only</h1>
-				{planets.map(
-					(planet, key) => planet.isGasPlanet && <h2>{planet.name}</h2>
-				)}
-
-				<h1>Is not a Gas Planet</h1>
-				{planets.map(
-					(planet, key) => !planet.isGasPlanet && <h2>{planet.name}</h2>
-				)}
-			</>
+			{age}
+			<br />
+			{/* <h2 id="age"></h2> */}
+			<button onClick={increaseAge}>Increase Age</button>
 		</div>
 	);
 }
