88ea77ebe3841cd8b9f25bcd31b10b4278017d8b
ginoongflores <florespaul161@gmail.com>
2023-02-02 12:48:33 +0800

    setup learning environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import react from "react";
-import { ReactDOM } from "react-dom";
+import { ReactDOM } from "react-dom/client";
 import { Practice } from "./practice";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
