294592e05d15f1458208310e8c6a4ffb13af20ab
ginoongflores <florespaul161@gmail.com>
2023-02-02 12:46:57 +0800

    setup learning environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,9 @@
-export const DoSomething = () => {
-	return <h1>Hello World</h1>;
-};
+import react from "react";
+import { ReactDOM } from "react-dom";
+import { Practice } from "./practice";
 
-// anonymous function
-<button
-	onClick={() => {
-		console.log("Hello World");
-	}}
-></button>;
-
-// working with conditionals using ternary operators
-
-let age = 20;
-
-// instead of this
-// if (age > 22) {
-// 	console.log(name);
-// } else {
-// 	name = "Paul";
-// 	console.log(name);
-// }
-
-// we do this for if-else
-name = age > 22 ? "Christian" : "Paul";
-// or much better this
-
-// using logical operators
-let name = age > 18 && "Christian"; // here name is equal to Christian and will only display it if the condition is true
-// the output will be "Christian"
-
-// in react we can use the same logic as above
-const Component = () => {
-	return age > 22 ? <h1>Christian</h1> : <h1>Paul</h1>;
-};
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Practice />
+	</React.StrictMode>
+);
