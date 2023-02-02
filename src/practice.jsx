a576ac2dae1d42ef4db198b3d771b7b674653177
ginoongflores <florespaul161@gmail.com>
2023-02-02 12:48:47 +0800

    added practice.jsx

---
+++ src/practice.jsx
@@ -0,0 +1,35 @@
+export const Practice = () => {
+	// anonymous function
+	<button
+		onClick={() => {
+			console.log("Hello World");
+		}}
+	></button>;
+
+	// working with conditionals using ternary operators
+
+	let age = 20;
+
+	// instead of this
+	// if (age > 22) {
+	// 	console.log(name);
+	// } else {
+	// 	name = "Paul";
+	// 	console.log(name);
+	// }
+
+	// we do this for if-else
+	name = age > 22 ? "Christian" : "Paul";
+	// or much better this
+
+	// using logical operators
+	let name = age > 18 && "Christian"; // here name is equal to Christian and will only display it if the condition is true
+	// the output will be "Christian"
+
+	return <Component />;
+};
+
+// in react we can use the same logic as above
+export const Component = () => {
+	return age > 22 ? <h1>Christian</h1> : <h1>Paul</h1>;
+};
