f4dec99906fcc760870d0b2b7ef7b63f0b5b8f69
ginoongflores <florespaul161@gmail.com>
2023-02-04 19:34:52 +0800

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Text } from "./Text";
 
 function App() {
 	const [showText, setShowText] = useState(false);
 
 	return (
 		<div className="App">
-			<button onClick={() => setShowText(!showText)}>Show Text</button>
+			<button
+				onClick={() => {
+					setShowText(!showText);
+				}}
+			>
+				Show Text
+			</button>
 
-			{showText && <h2>Hello World</h2>}
+			{/* the Text component will only be shown if the condition is true. The display of the Text component in our App component is an act of mounting. And clicking the button again to hide the Text component is an act of unmounting */}
+			{showText && <Text />}
 		</div>
 	);
 }
