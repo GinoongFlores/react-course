a9e1843c4f6e37267f1c8411ea17aa17f83403be
ginoongflores <florespaul161@gmail.com>
2023-02-06 10:04:43 +0800

    run the exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Sample1 } from "./Sample1";
+import { Exercise } from "./Exercise";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	// React.StrictMode helps us to right better code by checking for problems in our code and giving us warnings in the console
 	<React.StrictMode>
-		<App />
+		{/* <App /> First example -fetching API about Cats */}
+		{/* <Sample1 /> */}
+		<Exercise />
 	</React.StrictMode>
 );
