9c6464014d04ebe5c6f446bc8f0af746a123146d
ginoongflores <florespaul161@gmail.com>
2023-02-06 17:00:54 +0800

    feat(useLoader): Data.jsx & App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,53 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App() {
-	return <div className="App"></div>;
-}
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Data, dataLoader } from "./Data";
 
-export default App;
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	Link,
+	Outlet,
+	RouterProvider,
+} from "react-router-dom";
+
+export const App = () => {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<Root />}>
+				<Route index element={<Home />} />
+				<Route path="/menu" element={<Menu />} />
+				<Route path="/data" element={<Data />} loader={dataLoader} />
+				<Route path="/contact" element={<Contact />} />
+			</Route>
+		)
+	);
+
+	return (
+		<div className="App">
+			<RouterProvider router={router} />
+		</div>
+	);
+};
+
+const Root = () => {
+	return (
+		<>
+			<div>
+				<Link to={"/"}>Home</Link>
+				<Link to={"/menu"}>Menu</Link>
+				<Link to={"/contact"}>Contact</Link>
+				<Link to={"/data"}>Data</Link>
+			</div>
+
+			<div>
+				{/* An <Outlet> should be used in parent route elements to render their child route elements */}
+				<Outlet />
+			</div>
+		</>
+	);
+};
