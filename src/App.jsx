47576447ba9bc55fd91ee921f842164ff26c3310
ginoongflores <florespaul161@gmail.com>
2023-02-06 17:39:18 +0800

    finalized new environment for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
 
 function App() {
 	return (
 		<div className="App">
-			<></>
+			<img src={reactLogo} className="App-logo" alt="logo" />
+
+			{/* Router specify where our routers are on our components or where can we access the react-router-dom in our components */}
+			<Router>
+				<Navbar />
+
+				{/* Routes define all the specific routes destination */}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/menu" element={<Menu />} />
+					<Route path="/contact" element={<Contact />} />
+
+					{/* we can add an asterisk to the path and an error message to the element to show none pages */}
+					<Route path="*" element={<h1>404: Not Found</h1>} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
