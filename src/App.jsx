fb85db22c36d139844c65cd3f856096c0159ee67
ginoongflores <florespaul161@gmail.com>
2023-02-07 12:03:55 +0800

    finalized react router samples

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,21 @@ import { Navbar } from "./Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const client = new QueryClient();
+const client = new QueryClient({
+	// as example the query is the read from the CRUD and the mutation is the CUD from the CRUD.
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false, // this stops the API from fetching again whenever we switch tabs
+		},
+	},
+});
 
 function App() {
 	return (
 		<div className="App">
 			<img src={reactLogo} className="App-logo" alt="logo" />
+
+			{/* here we wrapped all the routes with the QueryClientProvider that also calls the client. It's the same thing as what we do in AppContextProvider  */}
 			<QueryClientProvider client={client}>
 				<Router>
 					<Navbar />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,15 @@ export const Home = () => {
 	// the first argument is the key or the unique id and the second argument is the function that will be called to fetch the data from the API or the database.
 
 	// the data here is the data that is returned from the API or the database. It acts as a state
-	const { data, isLoading, isError } = useQuery(["cat"], () => {
-		return Axios.get("https://catfact.ninja/facts").then((res) => res.data);
+	const {
+		// we can change the data object by adding a name into it. In this case, we changed the data object to catData.
+		data: catData,
+		isLoading,
+		isError,
+		refetch,
+	} = useQuery(["cat"], () => {
+		// the refetch is a function that will be called to refetch or call the data again from the API.
+		return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
 	});
 
 	// the isLoading is a boolean value that tells if the data is still loading or not. It sets to true in this case.
@@ -23,7 +30,8 @@ export const Home = () => {
 			<h1>Welcome to the Home Page</h1>
 
 			{/* we need to add a question mark (?) on the data as the data is null initially. Adding, the ? tells to access the fact only if the data is not null. */}
-			<p>{data?.fact}</p>
+			<p>{catData?.fact}</p>
+			<button onClick={refetch}>Update</button>
 		</header>
 	);
 };
