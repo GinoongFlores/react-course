a3a4804f6738473636b1c16e622ebef83fb7fa8a
ginoongflores <florespaul161@gmail.com>
2023-02-07 12:12:54 +0800

    refactor: remove unused codes

--- src/App.jsx
+++ src/App.jsx
@@ -41,25 +41,3 @@ function App() {
 }
 
 export default App;
-
-// here we doing a thing called prop drilling which is not a good practice because the data is passed down from a higher-level component to a lower-level component through intermediate components that don't use the data. This makes the code harder to read and maintain.
-// const TopComponent = () => {
-// 	const [state, setState] = useState();
-// 	return (
-// 		<div>
-// 			<MiddleComponent state={state} />
-// 		</div>
-// 	);
-// };
-
-// const MiddleComponent = (state) => {
-// 	return (
-// 		<div>
-// 			<BottomComponent state={state} />
-// 		</div>
-// 	);
-// };
-
-// const BottomComponent = (state) => {
-// 	return <div>{state}</div>;
-// };
